4e37abeb1375117bf44fbbcd47a69d686aa3531d
Jake Walker <hi@jakew.me>
2020-10-01 12:55:53 +0100

    Fix nav buttons

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,11 +3,9 @@
     <div id="header" class="bg-gray-300 dark:bg-gray-900">
       <div class="container mx-auto text-center py-10">
         <g-image alt="Jake Walker Avatar" class="inline" src="~/assets/avatar.png" width="150" height="150" fit="inside"/>
-        <g-link to="/">
-          <h1 class="text-5xl tracking-tight font-bold bg-gradient-to-r from-primary to-secondary bg-clip-text bg-repeat" id="site-title">{{ $static.metadata.siteName }}</h1>
-        </g-link>
-        <ul class="flex justify-center">
-          <li class="mr-3" v-for="item in nav" :key="item.name">
+        <h1 class="text-5xl tracking-tight font-bold bg-gradient-to-r from-primary to-secondary bg-clip-text bg-repeat" id="site-title">{{ $static.metadata.siteName }}</h1>
+        <ul class="flex justify-center my-2">
+          <li class="mx-4" v-for="item in nav" :key="item.name">
             <g-link class="nav-link text-primary text-lg" exact-active-class="text-secondary underline" :to="item.path">{{ item.name }}</g-link>
           </li>
         </ul>
