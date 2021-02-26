5f82715a71c1aa1bddd6380039d854a66fe5de0b
Jake Walker <hi@jakew.me>
2021-02-26 17:24:37 +0000

    Add Shynet Analytics

--- src/main.js
+++ src/main.js
@@ -8,5 +8,14 @@ import "fontsource-work-sans";
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
+
+  // Analytics
+  head.script.push({
+    src: "https://shynet.jakewalker.xyz/ingress/db7e13ba-9f5d-492c-a946-289a74e7ab00/script.js",
+    body: true
+  });
+  head.noscript.push({
+    innerHtml: "<img src='https://shynet.jakewalker.xyz/ingress/db7e13ba-9f5d-492c-a946-289a74e7ab00/pixel.gif'>"
+  });
 }
